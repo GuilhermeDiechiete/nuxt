fb5823828d59e3e0ec77155035657fdc09503bc9
Diechiete <diechiete99@gmail.com>
2026-05-18 21:18:13 -0300

    Upgrade PageCategoriesSummary

--- client/app/components/summary/SummaryCategories.vue
+++ client/app/components/summary/SummaryCategories.vue
@@ -16,6 +16,7 @@ watch(
   }
 )
 
+
 const formatCurrency = (value: number) => {
   return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
@@ -24,14 +25,20 @@ const formatCurrency = (value: number) => {
 }
 </script>
 <template>
-  <div class="space-y-2">
+  <UPageCard>
+    <div>
+      <UIcon name="i-lucide-chart-column" class="text-primary mr-2" />
+      <span class="font-semibold italic mr-4 text-gray-400">
+        Categorias
+      </span>
+      <YearSelected />
+    </div>
     <!-- HEADER -->
-    <UCard class="border border-gray-800 bg-gray-900">
+    <UCard class="border border-gray-800 bg-gray-900" :ui="{ body: 'p-2 sm:p-2' }">
       <div class="flex items-center justify-between">
 
         <div class="flex items-center">
           <p class="text-primary mr-2"> Entradas: Receitas</p>
-          <YearSelected />
         </div>
     
         <div class="flex items-center gap-4">
@@ -54,35 +61,34 @@ const formatCurrency = (value: number) => {
     <CategoryTableInputsSummary />
     
     <!-- HEADER -->
-    <UCard class="border border-gray-800 bg-gray-900">
+    <UCard class="border border-gray-800 bg-gray-900 mt-10" :ui="{ body: 'p-2 sm:p-2' }">
       <div class="flex items-center justify-between">
 
-        <div class="flex items-center">
-          <p class="font-semibold text-error mr-2"> Despesas</p>
-          <YearSelected />
-        </div>
+<div class="flex items-center">
+  <p class="flex items-center gap-1 italic text-red-400">
+    <UIcon name="i-lucide-arrow-down" class="text-red-400" />
+    Saídas
+  </p>
+</div>
 
         <div class="flex items-center gap-4">
-          <p class="italic text-error">
-            Fixas: {{  formatCurrency(reportStore.totalOutputsFixedYear) }}
+          <p class="italic text-gray-400">
+            Fixo a.a: <span class="text-red-400">{{  formatCurrency(reportStore.totalOutputsFixedYear) }}</span>
             </p>
-            <p class="italic text-error">
-            Variavel: {{  formatCurrency(reportStore.totalOutputsVariableYear) }}
+            <p class="italic text-gray-400">
+            Variaáveis a.a: <span class="text-red-400">{{  formatCurrency(reportStore.totalOutputsVariableYear) }}</span>
           </p>
      
         </div>
 
         <div class="flex items-center gap-4">
           <p class="italic text-error">
-            Total: {{ formatCurrency(reportStore.totalOutputsYear) }}
+            {{ formatCurrency(reportStore.totalOutputsYear) }}
           </p>
         </div>
       </div>
     </UCard>
 
     <CategoryTableOutputsSummary />
-   
-
-  </div>
-
+  </UPageCard>
 </template>
--- client/app/components/tables/CategoryTableOutputsSummary.vue
+++ client/app/components/tables/CategoryTableOutputsSummary.vue
@@ -1,28 +1,53 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import {
+  computed,
+  watch,
+  ref,
+  h,
+  resolveComponent
+} from 'vue'
+
 import type { TableColumn } from '@nuxt/ui'
-import { useReportStore } from '#imports'
+import type { Column } from '@tanstack/vue-table'
+
+import {
+  useReportStore,
+  useGlobalStore
+} from '#imports'
+
+const UButton = resolveComponent('UButton')
+const UDropdownMenu = resolveComponent('UDropdownMenu')
 
 const reportStore = useReportStore()
+const globalStore = useGlobalStore()
+
+watch(
+  () => globalStore.year,
+  async () => {
+    await reportStore.fetchReports()
+  }
+)
 
 type TotalByMonth = {
-    1: number
-    2: number
-    3: number
-    4: number
-    5: number
-    6: number
-    7: number
-    8: number
-    9: number
-    10: number
-    11: number
-    12: number
+  1: number
+  2: number
+  3: number
+  4: number
+  5: number
+  6: number
+  7: number
+  8: number
+  9: number
+  10: number
+  11: number
+  12: number
 }
+
 type Categories = {
   id: number
   category: string
   category_name: string
+
   months: {
     1: number
     2: number
@@ -37,6 +62,7 @@ type Categories = {
     11: number
     12: number
   }
+
   totalYear: number
 }
 
@@ -52,125 +78,317 @@ const formatCurrency = (value: number) => {
   return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL'
-  }).format(value)
+  }).format(value || 0)
+}
+
+function getHeader(
+  column: Column<Categories>,
+  label: string
+) {
+  const isSorted = column.getIsSorted()
+
+  return h(
+    UDropdownMenu,
+    {
+      content: {
+        align: 'start'
+      },
+
+      items: [
+        {
+          label: 'Asc',
+          type: 'checkbox',
+
+          icon: 'i-lucide-arrow-up-narrow-wide',
+
+          checked: isSorted === 'asc',
+
+          onSelect: () => {
+            if (isSorted === 'asc') {
+              column.clearSorting()
+            } else {
+              column.toggleSorting(false)
+            }
+          }
+        },
+
+        {
+          label: 'Desc',
+          type: 'checkbox',
+
+          icon: 'i-lucide-arrow-down-wide-narrow',
+
+          checked: isSorted === 'desc',
+
+          onSelect: () => {
+            if (isSorted === 'desc') {
+              column.clearSorting()
+            } else {
+              column.toggleSorting(true)
+            }
+          }
+        }
+      ]
+    },
+
+    () =>
+      h(UButton, {
+        color: 'neutral',
+
+        variant: 'ghost',
+
+        label,
+
+        icon: isSorted
+          ? isSorted === 'asc'
+            ? 'i-lucide-arrow-up-narrow-wide'
+            : 'i-lucide-arrow-down-wide-narrow'
+          : 'i-lucide-arrow-up-down',
+
+        class: '-mx-2.5 text-gray-400 italic'
+      })
+  )
 }
 
 const columns: TableColumn<Categories>[] = [
   {
     accessorKey: 'category',
-    header: 'Categoria',
+
+    header: ({ column }) =>
+      getHeader(column, 'Categoria'),
+
     footer: () => 'TOTAL'
   },
 
   {
     accessorKey: 'category_name',
-    header: 'Nome',
+
+    header: ({ column }) =>
+      getHeader(column, 'Nome'),
+
     footer: () => '-'
   },
 
   {
     id: 'jan',
-    header: 'Janeiro',
-    cell: ({ row }) => formatCurrency(row.original.months[1] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[1])
+
+    accessorFn: (row) => row.months[1],
+
+    header: ({ column }) =>
+      getHeader(column, 'Janeiro'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[1]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[1])
   },
 
   {
     id: 'fev',
-    header: 'Fevereiro',
-    cell: ({ row }) => formatCurrency(row.original.months[2] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[2])
+
+    accessorFn: (row) => row.months[2],
+
+    header: ({ column }) =>
+      getHeader(column, 'Fevereiro'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[2]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[2])
   },
 
   {
     id: 'mar',
-    header: 'Março',
-    cell: ({ row }) => formatCurrency(row.original.months[3] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[3])
+
+    accessorFn: (row) => row.months[3],
+
+    header: ({ column }) =>
+      getHeader(column, 'Março'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[3]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[3])
   },
 
   {
     id: 'abr',
-    header: 'Abril',
-    cell: ({ row }) => formatCurrency(row.original.months[4] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[4])
+
+    accessorFn: (row) => row.months[4],
+
+    header: ({ column }) =>
+      getHeader(column, 'Abril'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[4]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[4])
   },
 
   {
     id: 'mai',
-    header: 'Maio',
-    cell: ({ row }) => formatCurrency(row.original.months[5] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[5])
+
+    accessorFn: (row) => row.months[5],
+
+    header: ({ column }) =>
+      getHeader(column, 'Maio'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[5]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[5])
   },
 
   {
     id: 'jun',
-    header: 'Junho',
-    cell: ({ row }) => formatCurrency(row.original.months[6] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[6])
+
+    accessorFn: (row) => row.months[6],
+
+    header: ({ column }) =>
+      getHeader(column, 'Junho'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[6]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[6])
   },
 
   {
     id: 'jul',
-    header: 'Julho',
-    cell: ({ row }) => formatCurrency(row.original.months[7] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[7])
+
+    accessorFn: (row) => row.months[7],
+
+    header: ({ column }) =>
+      getHeader(column, 'Julho'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[7]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[7])
   },
 
   {
     id: 'ago',
-    header: 'Agosto',
-    cell: ({ row }) => formatCurrency(row.original.months[8] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[8])
+
+    accessorFn: (row) => row.months[8],
+
+    header: ({ column }) =>
+      getHeader(column, 'Agosto'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[8]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[8])
   },
 
   {
     id: 'set',
-    header: 'Setembro',
-    cell: ({ row }) => formatCurrency(row.original.months[9] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[9])
+
+    accessorFn: (row) => row.months[9],
+
+    header: ({ column }) =>
+      getHeader(column, 'Setembro'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[9]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[9])
   },
 
   {
     id: 'out',
-    header: 'Outubro',
-    cell: ({ row }) => formatCurrency(row.original.months[10] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[10])
+
+    accessorFn: (row) => row.months[10],
+
+    header: ({ column }) =>
+      getHeader(column, 'Outubro'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[10]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[10])
   },
 
   {
     id: 'nov',
-    header: 'Novembro',
-    cell: ({ row }) => formatCurrency(row.original.months[11] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[11])
+
+    accessorFn: (row) => row.months[11],
+
+    header: ({ column }) =>
+      getHeader(column, 'Novembro'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[11]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[11])
   },
 
   {
     id: 'dez',
-    header: 'Dezembro',
-    cell: ({ row }) => formatCurrency(row.original.months[12] || 0),
-    footer: () => formatCurrency(totalsByMonth.value[12])
+
+    accessorFn: (row) => row.months[12],
+
+    header: ({ column }) =>
+      getHeader(column, 'Dezembro'),
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.months[12]),
+
+    footer: () =>
+      formatCurrency(totalsByMonth.value[12]),
+      
   },
 
   {
     accessorKey: 'totalYear',
-    header: 'Total a.a',
+
+    header: ({ column }) =>
+      getHeader(column, 'Total A.A'),
+
     meta: {
       class: {
         th: 'text-right',
-        td: 'text-right font-bold text-error',
+
+        td: 'text-right text-red-400'
       }
     },
-    cell: ({ row }) => formatCurrency(row.original.totalYear),
-    footer: () => formatCurrency(reportStore.totalOutputsYear)
+
+    cell: ({ row }) =>
+      formatCurrency(row.original.totalYear),
+
+    footer: () =>
+      formatCurrency(reportStore.totalOutputsYear)
   }
 ]
+
+const sorting = ref([
+  {
+    id: 'totalYear',
+    desc: true
+  }
+])
 </script>
 
 <template>
   <UTable
+    v-model:sorting="sorting"
     :data="data"
     :columns="columns"
-    class="flex-1 text-xs"
+
+    :ui="{
+      th: 'text-red-400',
+      td: 'text-gray-400',
+    }"
   />
 </template>
